ad72525d0a1eaa4f55a27a2f05c504b7da417a9c
johnoro <officialjohnorourke@gmail.com>
2024-10-28 21:29:12 -0400

    cleans up unused vite-generated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 
 const todoList = [];
@@ -11,7 +9,7 @@ const todoList = [];
 ].forEach((title, id) => { todoList.push({id, title}) });
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
