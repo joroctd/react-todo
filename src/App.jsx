0babad721617ec775a8e6ec20ffddaed5bae0a00
johnoro <officialjohnorourke@gmail.com>
2024-10-28 21:23:21 -0400

    completes l.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const todoList = [];
+[
+  'Master React', 
+  'Master Node', 
+  'Clear out email'
+].forEach((title, id) => { todoList.push({id, title}) });
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Todo List</h1>
+      <ul className='todo-list'>
+        {
+          todoList.map(({title, id}) => (
+            <li
+              key={id}
+            >
+              {title}
+            </li>
+          ))
+        }
+      </ul>
     </>
   )
 }
