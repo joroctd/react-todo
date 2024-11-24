f512d2a7dc1588981fc8410419f933f108cf5a67
johnoro <officialjohnorourke@gmail.com>
2024-11-23 20:22:24 -0500

    mimics async API call and adds isLoading state

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,34 @@ import './App.css';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-function useSemiPersistentState() {
+function App() {
 	const TODO_LIST_KEY = 'savedTodoList';
 	const localTodoList = JSON.parse(localStorage.getItem(TODO_LIST_KEY));
 	const [todoList, setTodoList] = useState(localTodoList || []);
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-		localStorage.setItem(TODO_LIST_KEY, JSON.stringify(todoList));
+		(async () => {
+			const { data } = await new Promise((resolve, reject) => {
+				setTimeout(() => {
+					resolve({
+						data: {
+							todoList
+						}
+					});
+				}, 2000);
+			});
+
+			setTodoList(data.todoList);
+			setIsLoading(false);
+		})();
+	}, []);
+
+	useEffect(() => {
+		if (!isLoading) {
+			localStorage.setItem(TODO_LIST_KEY, JSON.stringify(todoList));
+		}
 	}, [todoList]);
-
-	return [todoList, setTodoList];
-}
-
-function App() {
-	const [todoList, setTodoList] = useSemiPersistentState();
 
 	const addTodo = newTodo => {
 		let id;
