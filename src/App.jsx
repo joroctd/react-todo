5b07b6b672b1f5cc5773c4e62d5794ecefe25bca
johnoro <officialjohnorourke@gmail.com>
2024-11-23 22:15:15 -0500

    refactors to use reducer hook to hold app state

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,78 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
+const todoListReducer = (state, action) => {
+	switch (action.type) {
+		case 'FETCH_INIT':
+			return {
+				...state,
+				isLoading: true,
+				isError: false
+			};
+		case 'FETCH_SUCCESS':
+			return {
+				...state,
+				isLoading: false,
+				isError: false,
+				data: action.payload
+			};
+		case 'FETCH_FAILURE':
+			return {
+				...state,
+				isLoading: false,
+				isError: true
+			};
+		case 'ADD':
+			return {
+				...state,
+				data: [...state.data, action.payload]
+			};
+		case 'REMOVE':
+			return {
+				...state,
+				data: state.data.filter(todo => todo.id !== action.payload.id)
+			};
+		default:
+			throw new Error('Unsupported todoList action type.');
+	}
+};
+
 function App() {
 	const TODO_LIST_KEY = 'savedTodoList';
 	const localTodoList = JSON.parse(localStorage.getItem(TODO_LIST_KEY));
-	const [todoList, setTodoList] = useState(localTodoList || []);
-	const [isLoading, setIsLoading] = useState(true);
+	const [todoList, dispatchTodoList] = useReducer(todoListReducer, {
+		data: localTodoList,
+		isLoading: false,
+		isError: false
+	});
 
 	useEffect(() => {
-		(async () => {
+		dispatchTodoList({ type: 'FETCH_INIT' });
+
+		const fetchTodos = async () => {
 			const { data } = await new Promise((resolve, reject) => {
 				setTimeout(() => {
 					resolve({
 						data: {
-							todoList
+							todos: todoList.data
 						}
 					});
 				}, 2000);
 			});
 
-			setTodoList(data.todoList);
-			setIsLoading(false);
-		})();
+			dispatchTodoList({ type: 'FETCH_SUCCESS', payload: data.todos });
+		};
+
+		fetchTodos();
 	}, []);
 
 	useEffect(() => {
-		if (!isLoading) {
-			localStorage.setItem(TODO_LIST_KEY, JSON.stringify(todoList));
+		if (!todoList.isLoading) {
+			localStorage.setItem(TODO_LIST_KEY, JSON.stringify(todoList.data));
 		}
-	}, [todoList]);
+	}, [todoList.data]);
 
 	const addTodo = newTodo => {
 		let id;
@@ -39,11 +81,12 @@ function App() {
 		} catch (e) {
 			id = Date.now();
 		}
-		setTodoList([...todoList, { title: newTodo, id }]);
+
+		dispatchTodoList({ type: 'ADD', payload: { title: newTodo, id } });
 	};
 
 	const removeTodo = id => {
-		setTodoList(todoList.filter(todo => todo.id !== id));
+		dispatchTodoList({ type: 'REMOVE', payload: { id } });
 	};
 
 	return (
@@ -51,14 +94,14 @@ function App() {
 			<h1>Todo List</h1>
 			<AddTodoForm
 				onAddTodo={addTodo}
-				isLoading={isLoading}
+				isLoading={todoList.isLoading}
 			/>
 			<hr />
-			{isLoading ? (
+			{todoList.isLoading ? (
 				<p>Loading...</p>
 			) : (
 				<TodoList
-					todoList={todoList}
+					todoList={todoList.data}
 					onRemoveTodo={removeTodo}
 				/>
 			)}
