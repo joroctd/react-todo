8d7c074b88f0c49c9deaf01201e39ff4c6c1938b
johnoro <officialjohnorourke@gmail.com>
2025-02-26 19:52:02 -0500

    adds potential fix for netlify routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ClerkProvider } from '@clerk/clerk-react';
 import './index.css';
 import App from '@/components/App';
@@ -11,12 +11,12 @@ if (!PUBLISHABLE_KEY) throw new Error('Missing Publishable Key');
 
 createRoot(document.getElementById('root') as HTMLElement).render(
 	<StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<ClerkProvider
 				publishableKey={PUBLISHABLE_KEY}
 				afterSignOutUrl='/'>
 				<App />
 			</ClerkProvider>
-		</BrowserRouter>
+		</HashRouter>
 	</StrictMode>
 );
